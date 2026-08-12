db255ca99793a01ddc95574d2131214fa9d8d033
Reinaldo Galvão <reinaldo@example.com>
2026-08-12 09:49:20 -0300

    Atualizar imagens_2ano.py

--- imagens_2ano.py
+++ imagens_2ano.py
@@ -87,6 +87,7 @@ st.set_page_config(page_title="Mural de Estudantes", layout="centered")
 
 st.title("📸 Mural de Fotos")
 st.subheader("Profª. Glauciana - 2º ano 2025")
+st.subheader("Colégio Losango")
 
 menu = st.radio("O que você deseja fazer?", ["Enviar Imagens", "Ver Galeria de Todos"], horizontal=True)
 
